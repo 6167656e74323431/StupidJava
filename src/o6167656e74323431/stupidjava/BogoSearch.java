78fdd8a6373da3d53e738ec582820b6d1afdea17
Theodore Preduta <tpreduta@gmail.com>
2019-04-11 22:41:32 -0400

    Added overlaods for primitive types

--- src/o6167656e74323431/stupidjava/BogoSearch.java
+++ src/o6167656e74323431/stupidjava/BogoSearch.java
@@ -7,47 +7,175 @@ import java.security.SecureRandom;
  *
  * @author     6167656e74323431
  *
- * @version    1.3
+ * @version    1.4
  */
 public final class BogoSearch
 {
-
 	/**
 	 * Private default constructor to prevent the creation of instances of the
 	 * BogoSearch class
 	 */
 	private BogoSearch()
-	{
-	}
+	{ /* \o> */ }
 
 	/**
 	 * A probabilistic function to see if a comparable object exists within an
 	 * array of comparable objects. The object equality is checked via
-	 * .compareTo() == 0
+	 * {@code .compareTo() == 0}
 	 *
 	 * @param      haystack  An array of comparable objects for this searching
 	 *                       method to search through
 	 * @param      needle    The object that is trying to be found in the
 	 *                       haystack
 	 *
-	 * @return     Returns true if the needle exists within the haystack, false
-	 *             if the needle is probably not in the haystack
-	 *
-	 * @throws     NullPointerException  If either the needle, or the haystack are
-	 *                                   equal to null
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T extends Comparable> boolean searchArray(T[] haystack, T needle)
+	public static <T extends Comparable> int searchArray(T[] haystack, T needle)
 	{
-		if (haystack[(new SecureRandom()).nextInt(haystack.length)].compareTo(needle) == 0)
-			return true;
 		try
 		{
+			int checkID = (new SecureRandom()).nextInt(haystack.length);
+			if (haystack[checkID].compareTo(needle) == 0)
+				return checkID;
 			return searchArray(haystack, needle);
 		}
 		catch (StackOverflowError e)
 		{
-			return false;
+			return -1;
+		}
+		catch (NullPointerException e)
+		{
+			throw new IllegalArgumentException("null argument.");
 		}
 	}
+
+	/**
+	 * Overloaded version of searchArray function to deal with the byte
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(byte[] haystack, byte needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for byte."); }
+
+	/**
+	 * Overloaded version of searchArray function to deal with the short
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(short[] haystack, short needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for short."); }
+
+	/**
+	 * Overloaded version of searchArray function to deal with the int
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(int[] haystack, int needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for int."); }
+
+	/**
+	 * Overloaded version of searchArray function to deal with the long
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(long[] haystack, long needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for long."); }
+
+	/**
+	 * Overloaded version of searchArray function to deal with the float
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(float[] haystack, float needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for float."); }
+
+	/**
+	 * Overloaded version of searchArray function to deal with the double
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(double[] haystack, double needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for double."); }
+
+	/**
+	 * Overloaded version of searchArray function to deal with the char
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(char[] haystack, char needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for char."); }
+
+	/**
+	 * Overloaded version of searchArray function to deal with the boolean
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.BogoSearch#searchArray(Comparable[],
+	 *             Comparable)
+	 *
+	 * @param      haystack  Array that contains the items to search through
+	 * @param      needle    The item to be searched for
+	 *
+	 * @return     Returns the array index if the item was found in the array,
+	 *             -1 otherwise.
+	 */
+	public static int searchArray(boolean[] haystack, boolean needle)
+	{ throw new UnsupportedOperationException("Search unimplemented for boolean."); }
 }
--- src/o6167656e74323431/stupidjava/MergeShuffle.java
+++ src/o6167656e74323431/stupidjava/MergeShuffle.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 import java.lang.reflect.Array;
 
 /**
- * This class provides a probabilistic a O(n*log(n)) method for shuffling arrays
- * using merge-sort as a framework for the algorithm.
+ * This class provides an O(n*log(n)) method for shuffling arrays using
+ * merge-sort as a framework for the algorithm.
  *
  * @author     6167656e74323431
  *
- * @version    1.1
+ * @version    1.2
  */
 public final class MergeShuffle
 {
@@ -19,8 +19,7 @@ public final class MergeShuffle
 	 * MergeShuffle class
 	 */
 	private MergeShuffle()
-	{
-	}
+	{ /* \o> */ }
 
 	/**
 	 * Function that uses a merge-sort style algorithm to sort an array of
@@ -31,29 +30,146 @@ public final class MergeShuffle
 	 *                     copied, and then shuffled.
 	 *
 	 * @return     A shuffled copy of the original array.
-	 *
-	 * @throws     NullPointerException  If target is equal to null.
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T[] shuffleArray(T[] target)
+	public static <T extends Object> T[] shuffleArray(T[] target)
 	{
-		if (target.length == 1)
-			return target;
-		T[] lowerTarget = Arrays.copyOfRange(target, 0, target.length / 2);
-		T[] upperTarget = Arrays.copyOfRange(target, target.length / 2, target.length);
-		lowerTarget = shuffleArray(lowerTarget);
-		upperTarget = shuffleArray(upperTarget);
-		T[] endArray = (T[])Array.newInstance(target[0].getClass(), target.length);
-		int endArrayPointer = 0, lowerTargetPointer = 0, upperTargetPointer = 0;
-		while (endArrayPointer < endArray.length)
-			if (lowerTargetPointer == lowerTarget.length)
-				endArray[endArrayPointer++] = upperTarget[upperTargetPointer++];
-			else if (upperTargetPointer == upperTarget.length)
-				endArray[endArrayPointer++] = lowerTarget[lowerTargetPointer++];
-			else if ((new SecureRandom()).nextDouble() < 0.5)
-				endArray[endArrayPointer++] = lowerTarget[lowerTargetPointer++];
-			else
-				endArray[endArrayPointer++] = upperTarget[upperTargetPointer++];
-		return endArray;
+		try
+		{
+			if (target.length == 1)
+				return target;
+			T[] lowerTarget = Arrays.copyOfRange(target, 0, target.length / 2);
+			T[] upperTarget = Arrays.copyOfRange(target, target.length / 2, target.length);
+			lowerTarget = shuffleArray(lowerTarget);
+			upperTarget = shuffleArray(upperTarget);
+			T[] endArray = (T[])Array.newInstance(target[0].getClass(), target.length);
+			int endArrayPointer = 0, lowerTargetPointer = 0, upperTargetPointer = 0;
+			while (endArrayPointer < endArray.length)
+				if (lowerTargetPointer == lowerTarget.length)
+					endArray[endArrayPointer++] = upperTarget[upperTargetPointer++];
+				else if (upperTargetPointer == upperTarget.length)
+					endArray[endArrayPointer++] = lowerTarget[lowerTargetPointer++];
+				else if ((new SecureRandom()).nextDouble() < 0.5)
+					endArray[endArrayPointer++] = lowerTarget[lowerTargetPointer++];
+				else
+					endArray[endArrayPointer++] = upperTarget[upperTargetPointer++];
+			return endArray;
+		}
+		catch (NullPointerException e)
+		{
+			throw new IllegalArgumentException("Argument must not be null");
+		}
 	}
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the byte
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static byte[] shuffleArray(byte[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for byte[]."); }
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the short
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static short[] shuffleArray(short[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for short[]."); }
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the int
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static int[] shuffleArray(int[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for int[]."); }
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the long
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static long[] shuffleArray(long[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for long[]."); }
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the float
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static float[] shuffleArray(float[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for float[]."); }
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the double
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static double[] shuffleArray(double[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for double[]."); }
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the char
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static char[] shuffleArray(char[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for char[]."); }
+
+	/**
+	 * Overloaded version of the shuffleArray function to deal with the boolean
+	 * primitive data-type. <br> <b>Note:</b> Unimplemented
+	 *
+	 * @see        o6167656e74323431.stupidjava.MergeShuffle#shuffleArray(Object[])
+	 *
+	 * @param      target  The original version of this array that will be
+	 *                     copied, and then shuffled.
+	 *
+	 * @return     A shuffled copy of the original array.
+	 */
+	public static boolean[] shuffleArray(boolean[] target)
+	{ throw new UnsupportedOperationException("Shuffle unimplemented for boolean[]."); }
 }
